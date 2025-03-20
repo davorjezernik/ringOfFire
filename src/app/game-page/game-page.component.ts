bff17874ff1eaa1b04d8a14d0e5430779ed507b5
Davor Jezernik <369davor@gmail.com>
2025-03-20 17:59:43 +0100

    uppdate

--- src/app/game-page/game-page.component.ts
+++ src/app/game-page/game-page.component.ts
@@ -1,18 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game-page',
-  imports: [CommonModule, Router],
+  imports: [CommonModule],
   templateUrl: './game-page.component.html',
   styleUrl: './game-page.component.scss'
 })
 export class GamePageComponent {
-  constructor(private router: Router) { }
 
-  newGame() {
-    this.router.navigateByUrl('/game');
-    //start game
-  }
 }
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+  constructor(private router: Router) { }
 
+  newGame() {
+    this.router.navigateByUrl('/game');
+    //start game
+  }
 }
